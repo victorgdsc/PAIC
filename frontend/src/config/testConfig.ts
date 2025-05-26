dabc7b75d9c9b2a18d3070c8174efdba09384cc0
victorgdsc <victorgomesdesac@gmail.com>
2025-05-26 13:18:54 -0300

    updated testConfig

--- frontend/src/config/testConfig.ts
+++ frontend/src/config/testConfig.ts
@@ -35,21 +35,16 @@ export const TEST_CONFIG = {
       description: ['Pedidos realizados na Olist entre 2016 e 2018, com dados sobre entregas, clientes, produtos e pagamentos.',
 'O arquivo possui 120.105 registros, o que melhora a confiabilidade das previsões. Por ser grande, pode demorar mais para carregar e deixar a navegação mais lenta.'],
       numericColumns: [
-          'price','freight_value','product_weight_g','product_length_cm','payment_installments','payment_value'
+          'price','freight_value','product_weight_g','payment_value'
         ],
         columnMapping: {
           'order_delivered_customer_date': 'actualDate',
           'order_estimated_delivery_date': 'estimatedDate',
-          'order_status': 'factor',
           'customer_city': 'factor',
-          'customer_state': 'factor',
           'seller_city': 'factor',
-          'seller_state': 'factor',
           'price': 'factor',
           'freight_value': 'factor',
-          'product_category_name_english': 'factor',
           'product_weight_g': 'factor',
-          'product_length_cm': 'factor',
           'payment_value': 'factor'
         }
       },
@@ -60,31 +55,14 @@ export const TEST_CONFIG = {
         description: ['Simulação de operações logísticas entre 2015 e 2018, com dados sobre pedidos, envios, clientes e categorias de produtos.',
           'O arquivo possui 180.519 registros, o que melhora a confiabilidade das previsões. Por ser grande, pode demorar mais para carregar e deixar a navegação mais lenta.'],
         numericColumns: [
-          'Benefit per order','Sales','Order Item Product Price','Order Item Profit Ratio','Order Item Quantity','Order Item Total','Order Profit Per Order','Product Price'
+          'Sales','Product Price'
         ],
         columnMapping: {
           'Days for shipping (real)': 'actualDate',
           'Days for shipment(scheduled)': 'estimatedDate',
-          'Benefit per order': 'factor',
           'Category Name': 'factor',
           'Customer City': 'factor',
-          'Customer Country': 'factor',
-          'Customer Segment': 'factor',
-          'Customer State': 'factor',
-          'Customer Zipcode': 'factor',
-          'Department Name': 'factor',
-          'Market': 'factor',
           'Order City': 'factor',
-          'Order Country': 'factor',
-          'Order Item Product Price': 'factor',
-          'Order Item Profit Ratio': 'factor',
-          'Order Item Quantity': 'factor',
-          'Sales': 'factor',
-          'Order Item Total': 'factor',
-          'Order Profit Per Order': 'factor',
-          'Order Region': 'factor',
-          'Order State': 'factor',
-          'Order Zipcode': 'factor',
           'Product Price': 'factor',
           'Shipping Mode': 'factor'
         }
